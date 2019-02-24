35a06a9193b138fedd327aa6371a40e61173320e
Viacheslav Volodko <killobatt@gmail.com>
2019-02-24 18:59:22 +0200

    Initial Commit

---
+++ TextClassification/TextClassification.h
@@ -0,0 +1,19 @@
+//
+//  TextClassification.h
+//  TextClassification
+//
+//  Created by Viacheslav Volodko on 2/24/19.
+//  Copyright © 2019 killobatt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TextClassification.
+FOUNDATION_EXPORT double TextClassificationVersionNumber;
+
+//! Project version string for TextClassification.
+FOUNDATION_EXPORT const unsigned char TextClassificationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TextClassification/PublicHeader.h>
+
+
